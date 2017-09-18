eb9cf6b1ba89070007b266fb76f5af3d50baa9b7
Alexmruth <alexmruth@gmail.com>
2017-09-18 15:21:35 +1000

    almost completed settings

--- app/src/main/java/com/example/alex/ass2android/MainActivity.java
+++ app/src/main/java/com/example/alex/ass2android/MainActivity.java
@@ -9,20 +9,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     final String PREFS_NAME = "AOP_PREFS";
     public static final String PREFS_KEY = "ADP_PREFS_String";
     RelativeLayout rl;
     Button settingsBtn;
+    TextClock homeClock;
+    TextView timeLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         settingsBtn = (Button) findViewById(R.id.settingsBtn);
+        homeClock = (TextClock) findViewById(R.id.homeClock);
+        timeLocation = (TextView) findViewById(R.id.timeLocation);
 
-        SharedPreferences prefs = getSharedPreferences("bgColour", MODE_PRIVATE);
+        SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
         String colour = prefs.getString("bgColour", "WHITE");
+        String timeZone = prefs.getString("timeZone", "Default");
 
         rl = (RelativeLayout)findViewById(R.id.mainRL);
         if(colour.equals("WHITE")) {
@@ -30,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         } else {
             rl.setBackgroundColor(Color.parseColor("#e5feff"));
         }
+
+        homeClock.setTimeZone(timeZone);
 
     }
     public void goToSettings(View view) {
--- app/src/main/java/com/example/alex/ass2android/Settings.java
+++ app/src/main/java/com/example/alex/ass2android/Settings.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextClock;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
@@ -19,72 +25,128 @@ public class Settings extends AppCompatActivity {
     public static final String PREFS_KEY = "ADP_PREFS_String";
 
 
+    Spinner timeZoneSetting;
     RelativeLayout rl;
     CheckBox bgSwitch;
+    TextClock homeClock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        // Defining widgets
+        // Defining widgets etc
         bgSwitch = (CheckBox) findViewById(R.id.bgSwitch);
         rl = (RelativeLayout)findViewById(R.id.testRL);
+        timeZoneSetting = (Spinner) findViewById(R.id.timeZoneSetting);
+        homeClock = (TextClock) findViewById(R.id.homeClockTest);
+        SharedPreferences settingsPrefs = getSharedPreferences("settings", MODE_PRIVATE);
 
-        // Retrieving sharedpreferences settings which have been stored dictating the colour of the background
-        SharedPreferences prefs = getSharedPreferences("bgColour", MODE_PRIVATE);
-        String colour = prefs.getString("bgColour", "WHITE");
+        /* Retrieving sharedpreferences settings which have been stored dictating the colour of the background
+        as well as saving the state of the checkbox, either checked or not.
+         */
+        String colour = settingsPrefs.getString("bgColour", "WHITE"); //string of background colour
+        boolean bgChecked = settingsPrefs.getBoolean("checked", false); // boolean of if bgSwitch is checked, true or false
 
-        SharedPreferences bgColourCB = getSharedPreferences("bgColourCB", MODE_PRIVATE);
-        String isChecked = bgColourCB.getString("bgColourChecked", "false");
-
-        if(isChecked.equals("true")) {
+        if (bgChecked == true) {
             bgSwitch.setChecked(true);
         } else {
             bgSwitch.setChecked(false);
         }
 
-
+        // If statement below sets background colour of the relative layout according to sharedpreferences settings
         if(colour.equals("WHITE")) {
             rl.setBackgroundColor(Color.parseColor("#ffffff"));
         } else {
             rl.setBackgroundColor(Color.parseColor("#e5feff"));
         }
 
-        
-
-
         bgSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences bgColourPrefs = getSharedPreferences("bgColour", MODE_PRIVATE);
-                SharedPreferences.Editor bgEditor = bgColourPrefs.edit();
+                SharedPreferences settingsPrefs = getSharedPreferences("settings", MODE_PRIVATE);
+                SharedPreferences.Editor sEditor = settingsPrefs.edit();
 
-                SharedPreferences bgColourCBPrefs = getSharedPreferences("bgColourCB", MODE_PRIVATE);
-                SharedPreferences.Editor bgColourEditorCB = bgColourCBPrefs.edit();
                 String colourSelected;
                 boolean checked;
 
+                /* if bgSwitch CheckBox is changed, then the colour selected is changed according to
+                checkbox state, and the following string and boolean values are stored. */
                 if (bgSwitch.isChecked()) {
                     colourSelected = "LIGHTBLUE";
-                    bgEditor.putString("bgColour", colourSelected);
-                    bgEditor.commit();
+                    sEditor.putString("bgColour", colourSelected);
                     checked = true;
-                    bgColourEditorCB.putBoolean("checked", checked);
-                    bgColourEditorCB.commit();
+                    sEditor.putBoolean("checked", checked);
+                    sEditor.commit();
                     rl.setBackgroundColor(Color.parseColor("#e5feff"));
-
                 } else {
                     colourSelected = "WHITE";
-                    bgEditor.putString("bgColour", colourSelected);
-                    bgEditor.commit();
+                    sEditor.putString("bgColour", colourSelected);
                     checked = false;
-                    bgColourEditorCB.putBoolean("checked", checked);
-                    bgColourEditorCB.commit();
+                    sEditor.putBoolean("checked", checked);
+                    sEditor.commit();
                     rl.setBackgroundColor(Color.parseColor("#ffffff"));
                 }
             }
         });
 
+        // Create an ArrayAdapter to apply my array of cities to the timezone settings spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.homeTzSetting, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        timeZoneSetting.setAdapter(adapter);
+
+        timeZoneSetting.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
+    }
+
+
+    class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            SharedPreferences settingsPrefs = getSharedPreferences("settings", MODE_PRIVATE);
+            SharedPreferences.Editor sEditor = settingsPrefs.edit();
+            String city = adapterView.getItemAtPosition(i).toString();
+            String timeZoneSet;
+
+            switch (city) {
+                case "Brisbane":
+                    timeZoneSet = "GMT+10";
+                    sEditor.putString("timeZone", timeZoneSet);
+                    sEditor.commit();
+                    break;
+                case "Sydney":
+                    timeZoneSet = "GMT+10";
+                    sEditor.putString("timeZone", timeZoneSet);
+                    sEditor.commit();
+                    break;
+                case "Melbourne":
+                    timeZoneSet = "GMT+10";
+                    sEditor.putString("timeZone", timeZoneSet);
+                    sEditor.commit();
+                    break;
+            }
+
+                if (city.equals("Brisbane") || city.equals("Sydney") || city.equals("Melbourne")
+                        || city.equals("Hobart") || city.equals("Canberra")) {
+                    timeZoneSet = "GMT+10";
+                    sEditor.putString("timeZone", timeZoneSet);
+                    sEditor.commit();
+                } else if (city.equals("Adelaide") || city.equals("darwin")) {
+                    timeZoneSet = "Australia/Adelaide";
+                    sEditor.putString("timeZone", timeZoneSet);
+                    sEditor.commit();
+                } else if (city.equals("Perth")) {
+                    timeZoneSet = "GMT+8";
+                    sEditor.putString("timeZone", timeZoneSet);
+                    sEditor.commit();
+                };
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
     }
 
 
@@ -93,6 +155,4 @@ public class Settings extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-
 }
